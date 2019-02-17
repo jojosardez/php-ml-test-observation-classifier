b332b8e9067cc92ff03e4a52425d6f98b2ee8cac
Angelito Sardez, Jr <jojosardez@gmail.com>
2019-02-17 15:59:23 +1100

    Modified console printing of the results.

--- test.php
+++ test.php
@@ -8,10 +8,16 @@ require __DIR__ . '/classification/observation.php';
 $classifier = new ObservationClassifier();
 
 $testObservation1 = 'she initiate interactions and conversations with trusted educators, and begin to recognise that they have a right to belong to many communities';
-var_dump($classifier->Classify($testObservation1));
+$testObservation1Outcome = $classifier->Classify($testObservation1)["Recommended Outcome"];
+echo 'Observation: "'.$testObservation1.'"'.PHP_EOL;
+echo 'Recommended outcome: '.$testObservation1Outcome.PHP_EOL.PHP_EOL;
 
 $testObservation2 = 'Sabrina is playful and is always reaching out for company and friendship';
-var_dump($classifier->Classify($testObservation2));
+$testObservation2Outcome = $classifier->Classify($testObservation2)["Recommended Outcome"];
+echo 'Observation: "'.$testObservation2.'"'.PHP_EOL;
+echo 'Recommended outcome: '.$testObservation2Outcome.PHP_EOL.PHP_EOL;
 
 $testObservation3 = 'Sabrina communicate her needs for comfort and assistance. She is playful and is always reaching out for company and friendship. She alse express an opinion in matters that affect them.';
-var_dump($classifier->Classify($testObservation3));
+$testObservation3Outcome = $classifier->Classify($testObservation3)["Recommended Outcome"];
+echo 'Observation: "'.$testObservation3.'"'.PHP_EOL;
+echo 'Recommended outcome: '.$testObservation3Outcome.PHP_EOL.PHP_EOL;
